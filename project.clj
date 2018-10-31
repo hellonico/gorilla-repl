c85652796808c8c9d6333e6d335fff7f1f835d89
Nicolas Modrzyk <nico@karabiner-software.com>
2018-10-31 20:09:50 +0900

    update to 2018

--- project.clj
+++ project.clj
@@ -2,22 +2,22 @@
 ;;;;
 ;;;; gorilla-repl is licenced to you under the MIT licence. See the file LICENCE.txt for full details.
 
-(defproject gorilla-repl "0.4.0"
+(defproject gorilla-repl "0.4.1-SNAPSHOT"
   :description "A rich REPL for Clojure in the notebook style."
   :url "https://github.com/JonyEpsilon/gorilla-repl"
   :license {:name "MIT"}
-  :dependencies ^:replace [[org.clojure/clojure "1.6.0"]
-                           [http-kit "2.1.18"]
-                           [ring/ring-json "0.3.1"]
-                           [cheshire "5.3.1"]
-                           [compojure "1.1.8"]
-                           [org.slf4j/slf4j-api "1.7.7"]
-                           [ch.qos.logback/logback-classic "1.1.2"]
+  :dependencies ^:replace [[org.clojure/clojure "1.9.0"]
+                           [http-kit "2.3.0"]
+                           [ring/ring-json "0.4.0"]
+                           [cheshire "5.8.1"]
+                           [compojure "1.6.1"]
+                           [org.slf4j/slf4j-api "1.7.25"]
+                           [ch.qos.logback/logback-classic "1.2.3"]
                            [gorilla-renderable "2.0.0"]
                            [gorilla-plot "0.1.4"]
                            [javax.servlet/servlet-api "2.5"]
-                           [grimradical/clj-semver "0.2.0" :exclusions [org.clojure/clojure]]
-                           [cider/cider-nrepl "0.10.2"]
-                           [org.clojure/tools.nrepl "0.2.12"]]
+                           [grimradical/clj-semver "0.3.0" :exclusions [org.clojure/clojure]]
+                           [cider/cider-nrepl "0.18.0"]
+                           [org.clojure/tools.nrepl "0.2.13"]]
   :main ^:skip-aot gorilla-repl.core
   :target-path "target/%s")
